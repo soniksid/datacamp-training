a38c3f9f8a7819bcc79633f54649c7491acdcca8
Siddharth Soni <>
2020-04-30 18:57:07 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,38 @@
+print("\nOUTPUT BEGINS")
+
+#  addition
+a = "Addition of 4 and 5 is "
+b = 4 + 5
+print(a + str(b))
+
+# exponentiation
+a = "exponentiation of 4 and 2 is "
+b = 4 ** 2
+print(a + str(b))
+
+# modulo
+a = "modulo of 18 and 7 is "
+b = 18%7
+print(a + str(b))
+
+
+# assigning variables
+height = 1.79
+weight = 68.7
+print(type(height))
+
+# calculation of BMI
+a = "BMI is "
+bmi = weight / height**2
+print(a + str(bmi))
+
+# to see the type of a variable
+print(type(bmi))
+
+# to calculate the amount of money after 7 years investing $100
+savings = "amount of money after 7 years investing in $100 is "
+growth_multiplier = 100*1.1**7
+print(savings + str(growth_multiplier))
+
+
+
