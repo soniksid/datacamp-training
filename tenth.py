dbfaff323c9f5c2268e3472177399cc043e0380b
Siddharth Soni <siddharthksoni21@gmail.com>
2020-06-12 00:22:19 +0530

    Tenth course complete

--- tenth.py
+++ tenth.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import pandas as pd
+from sqlalchemy import create_engine 
 
 
 # file = open('datacamp_sample_file.txt', mode='r') # opening the file
@@ -22,9 +23,31 @@ import pandas as pd
 
 
 # loading excel file using pandas
-file = 'AirQuality.xlsx'
-data = pd.ExcelFile(file)
-print(data.sheet_names)
-df = data.parse('Sheet1')
-print(df)
+# file = 'AirQuality.xlsx'
+# data = pd.ExcelFile(file)
+# print(data.sheet_names)
+# df = data.parse('Sheet1')
+# print(df)
+
+
+# extracting data from database
+# engine = create_engine('sqlite:///database.sqlite')
+# names = engine.table_names()
+# print(names)
+
+
+# making sqlite file into dataframe
+# engine1 = create_engine('sqlite:///database.sqlite')
+# con = engine1.connect()
+# rs = con.execute("SELECT * FROM Match")
+# df = pd.DataFrame(rs.fetchall())
+# df.columns = rs.keys() #making columns names 
+# con.close()
+# print(df.head())
+
+
+#simple code to make sql file into a dataframe
+engine1 = create_engine('sqlite:///database.sqlite')
+df = pd.read_sql_query("SELECT * FROM Match", engine1)
+print(df.head())
 
