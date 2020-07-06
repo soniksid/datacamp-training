bbc28632908b59e1d048b76667f9895384c8db89
Siddharth Soni <siddharthksoni21@gmail.com>
2020-07-06 19:51:44 +0530

    Seventeen course complete

--- Statistical Thinking Part 2/Linear_plots.py
+++ Statistical Thinking Part 2/Linear_plots.py
@@ -11,65 +11,65 @@ df = census.groupby('State name').mean().reset_index()
 print(df)
 
 
-# # plotting scatter plot
-# plt.plot(df['Female'], df['Male'], marker='.', linestyle= 'none')
-# plt.xlabel('Female')
-# plt.ylabel('Male')
+# plotting scatter plot
+plt.plot(df['Female'], df['Male'], marker='.', linestyle= 'none')
+plt.xlabel('Female')
+plt.ylabel('Male')
 
-# # finding slope(a) and intercept(b) using np.polyfit()
-# a, b = np.polyfit(df['Female'], df['Male'], 1)
+# finding slope(a) and intercept(b) using np.polyfit()
+a, b = np.polyfit(df['Female'], df['Male'], 1)
 
-# # making theoretical line to plot (f = a * i + b)
-# x = np.array([0, 5899900])
-# y = a * x + b
-# plt.plot(x, y)
-# plt.show()
+# making theoretical line to plot (f = a * i + b)
+x = np.array([0, 5899900])
+y = a * x + b
+plt.plot(x, y)
+plt.show()
 
 
 # plotting ECDF plots using bootstrap methods
 # making ecdf function
-# def ecdf(data):
-#     """Compute ECDF for a one-dimensional array of measurements."""
-#     # Number of data points: n
-#     n = len(data)
+def ecdf(data):
+    """Compute ECDF for a one-dimensional array of measurements."""
+    # Number of data points: n
+    n = len(data)
 
-#     # x-data for the ECDF: x
-#     x = np.sort(data)
+    # x-data for the ECDF: x
+    x = np.sort(data)
 
-#     # y-data for the ECDF: y
-#     y = np.arange(1, n+1) / n
+    # y-data for the ECDF: y
+    y = np.arange(1, n+1) / n
 
-#     return x, y
+    return x, y
 
-# for _ in range(100):
-# 	df_sample = np.random.choice(df['Male'], size=len(df['Male']))
+for _ in range(100):
+	df_sample = np.random.choice(df['Male'], size=len(df['Male']))
 
-# x, y = ecdf(df_sample)
-# plt.plot(x, y, marker= '.', linestyle= 'none', alpha=0.1, color = 'gray')
+x, y = ecdf(df_sample)
+plt.plot(x, y, marker= '.', linestyle= 'none', alpha=0.1, color = 'gray')
 
-# # making another ecdf plot without bootstrap method
-# x, y = ecdf(df['Male'])
-# plt.plot(x, y, marker='.')
-# plt.margins(0.02)
-# plt.xlabel('Male population')
-# plt.ylabel('ECDF')
-# plt.show()
-
-
-# # defining a bootstrap function with
-# def bootstrap(data, func):
-# 	df_sample = np.random.choice(data, len(data))
-# 	return func(df_sample)
+# making another ecdf plot without bootstrap method
+x, y = ecdf(df['Male'])
+plt.plot(x, y, marker='.')
+plt.margins(0.02)
+plt.xlabel('Male population')
+plt.ylabel('ECDF')
+plt.show()
 
 
-# #making bootstrap replicates using for loops
-# df_replicas = np.empty(10000) # making empty array to create 10,000 replicates  
-# for i in range(10000):
-# 	df_replicas[i] = bootstrap(df['Male'], np.mean)
+# defining a bootstrap function with
+def bootstrap(data, func):
+	df_sample = np.random.choice(data, len(data))
+	return func(df_sample)
 
-# # potting histogram of bootstrap replicates
-# plt.hist(df_replicas, bins = 50)
-# plt.show()
+
+#making bootstrap replicates using for loops
+df_replicas = np.empty(10000) # making empty array to create 10,000 replicates  
+for i in range(10000):
+	df_replicas[i] = bootstrap(df['Male'], np.mean)
+
+# potting histogram of bootstrap replicates
+plt.hist(df_replicas, bins = 50)
+plt.show()
 
 
 # making a function for bootstrap pair returning slope and intercept
